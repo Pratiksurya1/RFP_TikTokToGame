f81ce1db78d8d891bd3b71241efc69b5683b4660
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-02-14 18:28:34 +0530

    Adding UC1 tik tak to bord

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTokToGame
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            TikTakTo_Bord tikTakTo=new TikTakTo_Bord();
+            tikTakTo.TikTakToBord();
+        }
+    }
+}
---
+++ TikTakToBord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTokToGame
+{
+    internal class TikTakTo_Bord
+    {
+        public void TikTakToBord()
+        {
+            Console.WriteLine("\t|\t|");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\t|\t|");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\t|\t|");
+        }
+    }
+}
