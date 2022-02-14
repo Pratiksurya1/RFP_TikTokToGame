7fc501876d578b3e960f81bae394180617766710
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-02-14 20:39:04 +0530

    Update

--- TikTakToBord.cs
+++ TikTakToBord.cs
@@ -11,16 +11,6 @@ namespace TikTokToGame
         char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         public void TikTakToBord()
         {
-            
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (i == 0)
-                {
-                    continue;
-                }
-                arr[i] = ' ';
-            }
-
             Console.WriteLine("{0}\t|{1}\t|{2}",arr[1],arr[2],arr[3]);
             Console.WriteLine("----------------------------");
             Console.WriteLine("{0}\t|{1}\t|{2}",arr[4],arr[5],arr[6]);
